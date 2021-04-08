0e4fccb7aa067ec4b7ade160a2e7f1fce2658a57
emmasarge <emmasargeant206@gmail.com>
2021-04-08 17:24:35 +0000

    fixed recipe.js

--- assets/js/recipe.js
+++ assets/js/recipe.js
@@ -1,6 +1,8 @@
-function getsource(id){
-$.ajax ({
-url:"https://api.spoonacular.com/recipes/information?apiKey=db254b5cd61744d39a2deebd9c361444",
+
+ 
+function getSource(id){
+$.ajax({
+url:"https://api.spoonacular.com/recipes/"+id+"/information?apiKey=3795c28ea918421d906d646b58835c6f",
 success: function(res) {
 
 document.getElementById("sourceLink").innerHTML=res.sourceUrl
@@ -8,13 +10,19 @@ document.getElementById("sourceLink").href=res.sourceUrl
 }
 });
 }
-function getRecipe(q) {
-$.ajax ({
-url:"https://api.spoonacular.com/recipes/search?apiKey=db254b5cd61744d39a2deebd9c361444&number=1&query="+q,
+function getRecipe(q){
+$.ajax({
+url:"https://api.spoonacular.com/recipes/search?apiKey=3795c28ea918421d906d646b58835c6f&number=1&query="+q,
 success: function(res) {
 
-document.getElementById("output").innerHTML="<h1>"+res.results[0].title+"</h1><br><img src='"+res.baseUri+res.results[0].image+"' width='400' /><br>Ready in "+res.results[0].readyInMinutes+" minutes"
-getsource(res.results[0].id)
+document.getElementById("output").innerHTML+="<h1>"+res.results[0].title+"</h1><br><img src='"+res.baseUri+res.results[0].image+"' width='400' />"
+getSource(res.results[0].id)
 }
 });
 }
+
+let input = document.querySelector('input');
+let recipe = document.querySelector('#output');
+input.addEventListener('change', function () {
+    recipe.textContent = recipe.value;
+        });
