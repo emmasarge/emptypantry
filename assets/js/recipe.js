9b99e26e3a0ab1f6c6a6e9345f6aa625f94f7129
emmasarge <emmasargeant206@gmail.com>
2021-04-20 14:06:47 +0000

    i removed the prompt indicating on the recipe page that users can only type one ingredient at a time at the moment and instead used the message in inner html and change the message when the button is pushed

--- assets/js/recipe.js
+++ assets/js/recipe.js
@@ -1,7 +1,7 @@
 // Get information from search input
-function recipeResults(){
+function recipeResults() {
     var meal = $('#search').val();
-    if (meal.length ==0) {
+    if (meal.length == 0) {
         alert("Please type an ingredient for your recipe search.");
     }
     else {
@@ -10,60 +10,58 @@ function recipeResults(){
     }
 }
 // Call to MealDB API to produce a list a recipes according to the search input
-function recipeSearch (mealname) {
-    var api ="https://www.themealdb.com/api/json/v1/1/search.php?s=" + mealname ;
+function recipeSearch(mealname) {
+    var api = "https://www.themealdb.com/api/json/v1/1/search.php?s=" + mealname;
     $('#meal').html("<img src='https://media.giphy.com/media/sSgvbe1m3n93G/giphy.gif' width='100px'/>");
-    
+
     var content = $('#meal');
 
-    $.getJSON(api,function(data) {
+    $.getJSON(api, function (data) {
         var recipe = data.meals;
 
         console.log(recipe);
 
-        var display="";
+        var display = "";
 
-        if (recipe==null) {
-            
-            $('#meal').html(`<p>Sorry, we cannot find a recipe with ${mealname} !</p>`);
+        if (recipe == null) {
+
+            $('#recipe-sub').html(`<p>Sorry, we cannot find a recipe with ${mealname} !</p>`);
         }
         else {
-             for (var i=0; i < recipe.length; i++) {
-                
-                display +=`<div id="recipe-wrapper">
+            for (var i = 0; i < recipe.length; i++) {
+
+                display += `<div id="recipe-wrapper">
                 <h2> ${recipe[i].strMeal}</h2>;
                <img src=${recipe[i].strMealThumb} width ='300px'/>
                <details ontoggle="showMethod()">
                 <summary>Click here for the recipe!</summary>
-             <p id="recipe"  >${recipe[i].strInstructions} </p>
+             <p id="recipe">${recipe[i].strInstructions} </p>
               </details>
                </div>
                `
-               ;
-               
-             content.html(display);
-           
+                    ;
+
+                content.html(display);
+
+            }
+
         }
-      
-    }
-});
+    });
 };
 // Changing the recipe results text to be bolder than what is called in the about.css
-function showMethod(){
-   $("#recipe").css("font-weight", "400");
+function showMethod() {
+    $("#recipe").css("font-weight", "400");
 
 }
 
 // This prompt lets users know this is a beta version of the website--Also trying to demonstrate if/else statement
-function betaMessage () {
-    let message = document.querySelector(".recipe-sub")
-    let recipePage = prompt("Our recipe database is in beta-mode. You can enter one ingredient at a time at the moment. If you are happy with this, type 'yes' and search for recipes to your heart's delight 🥦! If you are not, type 'no' to send us an email.");
-    if (recipePage.toLowerCase() === "no") {
-          location.href = "https://emmasarge.github.io/emptypantry/contact.html";
-        } else {
-          message.innerHTML =
-            "Enjoy your recipe search!";
-        }
-      }
+function openMessage() {
+    let mealname = $('#search').val();
+    if (meal.length == 0) {
+        $(".recipe-sub").replaceWith("");
+    }
+    else {
+       $('#recipe-sub').html(`<p>Please enjoy the following recipes with <strong>${mealname}</strong>!</p>`);
+    }
+}
 
-window.addEventListener("load", betaMessage);
